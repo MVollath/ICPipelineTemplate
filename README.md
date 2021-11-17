f7a30ec86689a5fbde0b88b189dea64f78406138
MVollath <maximilian.vollath@gmail.com>
2021-11-17 12:36:24 +0100

    The real initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# PipelineTemplate
+Template for the user submission for ICanalysis
+
+# requirements.txt
+This already contains all libraries that will be installed on our containers by default. You may add new libraries.
+You may remove the existing content of the file but beware of adding dependencies that aren't compatible with the existing ones.
+
+# solution.py
---
+++ segmentation.py
@@ -0,0 +1,31 @@
+'''
+The following third-party libraries are installed by default in our runtime environment:
+    numpy == 1.19.5
+    opencv-python ==4.5.3.56
+    ...
+If you need other packages, make sure you declare in the requirements.txt file
+'''
+
+# TODO: import packages
+
+
+class Solution:
+
+    def segmentation(self, image):
+        """
+            :type image: <class 'numpy.ndarray'>
+                This will already be a copy. You may modify it.
+            :rtype: <class 'numpy.ndarray'>
+        """
+        # TODO: implement your algorithm here
+        # The below code performs a very simple threshholding task
+
+        threshold = 128
+
+        # set all values above the threshold to 255
+        image[image >= threshold] = 255
+
+        # set all values below the threshold to 0
+        image[image < threshold] = 0
+
+        return image
